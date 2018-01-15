148c7c152c34f733ffee5295ce227d81d8b8a3fd
piraan <piraanj@gmail.com>
2018-01-15 23:58:43 +0700

    change true url

--- src/providers/api-url.ts
+++ src/providers/api-url.ts
@@ -1,2 +1,2 @@
-export const API_URL = '/api/v1';
-// export const API_URL = 'http://babylinlinsworld.com/refuze/api/v1';
+// export const API_URL = '/api/v1';
+export const API_URL = 'http://babylinlinsworld.com/refuze/api/v1';
